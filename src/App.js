a45846849f2ccc1bc63a9405fa62c907ce607192
furkandura <durafurkan@gmail.com>
2023-02-17 13:11:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {Admin, ListGuesser, Resource, CustomRoutes} from "react-admin";
+import simpleRestProvider from "ra-data-simple-rest";
+import {Route} from "react-router-dom";
+import Login from "./components/pages/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import authProvider from './authProvider';
+
+const dataProvider = simpleRestProvider("http://one-click-api.local/api");
+
+const App = () => (
+    <>
+        <Admin dataProvider={dataProvider} loginPage={<Login/>} authProvider={authProvider}>
+
+            <CustomRoutes>
+                <Route path="/register" element={<Login />} />
+            </CustomRoutes>
+        </Admin>
+    </>
+);
 
 export default App;
---
+++ src/components/pages/Login.js
@@ -0,0 +1,15 @@
+import { Card, CardContent } from '@mui/material';
+import { Title } from 'react-admin';
+
+function Login() {
+    return (
+        <Card>
+            <Title title="My Page" />
+            <CardContent>
+                ...
+            </CardContent>
+        </Card>
+    );
+}
+
+export default Login;
